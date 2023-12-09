c20924f5435f97170a5c6b8fd3d2c303f8914976
Meldrick Carabeo <meldrick.carabeo@gmail.com>
2023-12-09 17:05:27 +0800

    Create UserApp.java

---
+++ UserApp.java
@@ -0,0 +1,18 @@
+public class UserApp {
+    public static void main(String[] args) {
+        Application app = Application.getInstance();
+
+        app.loginUser("Juan");
+
+        app.loginUser("Maria");
+
+        app.logoutUser();
+
+        app.loginUser("Maria");
+
+        System.out.println("Next queue number: " + app.getNextQueueNumber());
+
+        app.resetQueueNumber(10);
+        System.out.println("Queue number after reset: " + app.getCurrentQueueNumber());
+    }
+}
